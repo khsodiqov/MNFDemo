d16a6d568e6ec64121cea654062a231c0e072af5
Khumoyunmirzo <khsodiqov@gmail.com>
2022-04-08 08:43:55 +0000

    Initial commit

---
+++ src/test/java/uz/inno/sd/MNFDemoTest.java
@@ -0,0 +1,21 @@
+package uz.inno.sd;
+
+import io.micronaut.runtime.EmbeddedApplication;
+import io.micronaut.test.extensions.junit5.annotation.MicronautTest;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
+
+import jakarta.inject.Inject;
+
+@MicronautTest
+class MNFDemoTest {
+
+    @Inject
+    EmbeddedApplication<?> application;
+
+    @Test
+    void testItWorks() {
+        Assertions.assertTrue(application.isRunning());
+    }
+
+}
